056def1585a3c17e2ce3fc77dc037253de8eeaa0
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2024-04-30 14:16:03 +0900

    백준 11720 : 숫자의 합
    배열

---
+++ Q001.cpp
@@ -0,0 +1,21 @@
+// 백준 11720 : 숫자의 합
+// 배열
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	int N;
+	string S;
+	int sum = 0;
+
+	cin >> N;
+	cin >> S;
+
+	for (int i = 0; i < N; i++) {
+		sum += S[i] - '0';
+	}
+
+	cout << sum;
+
+}
